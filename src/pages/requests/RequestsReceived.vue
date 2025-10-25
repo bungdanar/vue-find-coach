eb8165f3e2bf4ef17779908b5c32ebf4226a6402
Bung Danar <danar.g.putera@gmail.com>
2025-10-25 18:31:41 +0700

    Finish: Filtering Requests for the Active Coach

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,51 @@
-<template>REQUEST</template>
+<script>
+import RequestItem from "@/components/requests/RequestItem.vue";
+import BaseCard from "@/components/ui/BaseCard.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    BaseCard, RequestItem
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests']
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests']
+    }
+  }
+})
+</script>
+
+<template>
+  <section>
+    <BaseCard>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <RequestItem v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message">
+        </RequestItem>
+      </ul>
+      <h3 v-else>You have not received any requests yet!</h3>
+    </BaseCard>
+  </section>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import coaches from './modules/coaches'
+import requests from './modules/requests'
 
 const store = createStore({
   modules: {
     coaches,
+    requests,
   },
   state() {
     return {
